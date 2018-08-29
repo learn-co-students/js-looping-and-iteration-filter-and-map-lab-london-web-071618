6e30e82fb4b20a0422daa411dfb63bab90e08205
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-08-29 09:41:17 +0100

    jo

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here:
+
+function driversWithRevenueOver(collection, revenue) {
+  return collection.filter(function(driver) {
+    return driver.revenue > revenue
+  })
+}
+
+function driverNamesWithRevenueOver(collection, revenue) {
+  let filteredDrivers = driversWithRevenueOver(collection, revenue);
+  return filteredDrivers.map(driver => driver.name)
+}
+
+function exactMatch(collection, obj) {
+  let objKey = Object.keys(obj)
+  return collection.filter(function(driver) {
+    return driver[objKey] === obj[objKey]
+  })
+}
+
+function exactMatch(collection, obj) {
+  let objKey = Object.keys(obj)
+  return collection.filter(function(driver) {
+    return driver[objKey] === obj[objKey]
+  })
+}
+
+function exactMatchToList(collection, obj) {
+  return exactMatch(collection, obj).map(driver => driver.name)
+}
